b32505bd0c25a844b6fe0709a8795cd22d7772ad
MI-TECHDLIN <ezechukwumiracle52@gmail.com>
2026-02-19 12:21:47 +0100

    new update on emailvalidator added a class for more clearity and local json

--- cliloginvalidator.py
+++ cliloginvalidator.py
@@ -2,77 +2,128 @@
 import re
 import hashlib
 import getpass
+import json
+
+class jsonfile:
+      def loadfile(self):
+            try:
+                  with open(self,'r') as self:
+                        return json.load(self)
+            except FileNotFoundError,json.JSONDecodeError:
+                  return['file not found']
+            
+      def savefile(self,file):
+            with open(file,'w') as file:
+                  return json.dump(self,file,indent=1)
+       
 
 # string checker
-def name(p):
-        while True:
-              if(re.match(r'[A-Za-z]+$',p,re.I)):
-                     return p
+class emailvalidator:
+      def name(self):
+         while True:
+              if(re.match(r'[A-Za-z]+$',self,re.I)):
+                     return self
               else:
                      k=input('Invalid name format: ')
                      if (re.match(r'[A-Za-z]',k,re.I)):
                             
                             
                             return k
+                     
 # agechecker
-def ager(n):
-       while True:
-        try:
-                k= int(n)
-                if(k>=1):
+      def ager(self):
+             while True:
+              try:
+                   k= int(self)
+                   if(k>=1):
                           return k
-                else: 
+                   else: 
                                    k=int(input('invalid Age: ') )
-        except ValueError:
+              except ValueError:
                 n =input('invalid Age format: ')
-               
+
 # emailcehcker
-def checkemail(email):
-       while True:
-              if(re.fullmatch(r'[A-Za-b0-9]+@gmail.com$',email,re.I)):
-                     return email
+      def checkemail(self):
+          while True:
+              if(re.fullmatch(r'[A-Za-b0-9]+@gmail.com$',self,re.I)):
+                     return self
               else:
                      k=input('Invalid email: ')
                      if (re.fullmatch(r'\w+@gmail.com$',k)):
                             
                             return k
-
 # passwordchecker
-def psdconfirm(t=""):       
-      con=input('confirm password: ')
-      while t!=con:     
+      def psdconfirm(self=""):       
+           con=input('confirm password: ')
+           while self!=con:     
                  con=input('invlaid password: ')
-      return con
+           return con
 
-# temporal database
-database=[]
+class Emailconstructor:
+        def __init__(self,firstname,lastname,email,temppassword,confirimpasssword,age,gender):
+              self.firstname=firstname
+              self.lastname=lastname
+              self.email=email
+              self.temppass=temppassword
+              self.confrimpass=confirimpasssword
+              self.age=age
+              self.gender=gender
+
+        def todict(self):
+           return{'UserName': {'firstname':self.firstname,'secondname':self.lastname},
+     'Email':self.email,
+
+     'Userpassword':hashlib.sha256(self.confrimpass.encode()).hexdigest(),
+      'userage':self.age,
+      'usergender':self.gender
+     }
+                    
+              
+
+
+
+def getuserdetails():
+        rvalid=emailvalidator
+        while True:
+         firstname=rvalid.name(input('First Name: '))
+         lastname=rvalid.name(input('Last Name: '))
+         email=rvalid.checkemail(input('enter a valid email: '))
+         temppassword=input('create passeord: ')
+         confrimedpassword=rvalid.psdconfirm(temppassword)
+         age=rvalid.ager(input('Age: '))
+         gender=input('Gender-Male/Female: ')
+
+         getall=Emailconstructor(firstname,lastname,email,temppassword,confrimedpassword,age,gender)
+         
+         
+       
+         return getall.todict()
+        
+
 
 # loop runeer
-while True:
-       firstname=name(input('First Name: '))
-       lastname=name(input('Last Name: '))
-       email=checkemail(input('enter a valid email: ')).lower()
-       temppassword=input('create passeord: ')
-       confrimedpassword=psdconfirm(temppassword)
-       age=ager(input('Age: '))
-       gender=input('Gender-Male/Female: ').capitalize()
+def main():
+       file='userdashboard.json'
+       jsondbs=jsonfile
+       dbs=jsondbs.loadfile(file)
+       dbs.append(getuserdetails())
+       jsondbs.savefile(dbs,file)
+       
+      
+       
 
-       user={'UserName': [firstname,lastname],
-     'Email':email,
-
-     'Userpassword':hashlib.sha256(confrimedpassword.encode()).hexdigest(),
-      'userage':age,
-      'usergender':gender
-     }
-       database.append(user)
-       anotherinput=input('Do  you want to input any other user(y/n):  ').capitalize()
+       
+        
+       # database.append(user)
+       # anotherinput=input('Do  you want to input any other user(y/n):  ').capitalize()
    
-       if(anotherinput != 'Y'):
-              break   
+       # if(anotherinput != 'Y'):
+       #        break   
+
+if __name__ == "__main__":
+      main()
 
 
-
-print(database)
 
        
     
