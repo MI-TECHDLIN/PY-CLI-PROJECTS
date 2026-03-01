45700eee8815d0b2cf23870a302cd00fb131b7ac
MI-TECHDLIN <ezechukwumiracle52@gmail.com>
2026-03-01 12:09:03 +0100

    made some abstraction in the class

--- clibank/bankconstructor.py
+++ clibank/bankconstructor.py
@@ -1,38 +1,12 @@
-
-
-
-class bankp:
+class  bankconc:
     def __init__(self,balance=0,transfer=0,deposit=0,withdrawl=0,description=""):
         self.balance=balance
         self.transfer=transfer
         self.deposit=deposit
         self.withdrawl=withdrawl
         self.description=description
-    def transfer(t=0):
-     
-     transfer,description=t.balance - t.transfer,s.description
-     return t.balance,description
+    def __str__(self):
+      return f'{self.balance},{self.transfer},{self.deposit},{self.withdrawl},{self.description}'
 
-    def Deposit(d=0):
-     
-     deposit=d.deposit
-     balance=d.balance + deposit
-     return balance,deposit
-
-    def withdrawl(w=0):  
-     withdrawl=w.withdrawl
-     balance=w.balance - withdrawl
-     return balance,withdrawl
     
-    
-
-            
-
-
-
-     
-        
-
-   
-
     
