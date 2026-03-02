774d8c3afe58fde7bb62bdb3680cdb253dbb4411
MI-TECHDLIN <ezechukwumiracle52@gmail.com>
2026-03-02 21:01:08 +0100

    encapsulated a little made use of walrus operator

--- clibank/main.py
+++ clibank/main.py
@@ -1,41 +1,44 @@
+balance=2500
+
 from bankconstructor import bankconc
-from bankfunction import bankfunc
 
 
 def userrequest(): 
     print(f'_________________(((((((Welcome to Cli Banking)))))))_______________')
-    balance=2500
+    global balance
     bank=bankconc(balance)
-    func=bankfunc
-    
+  
     
     while True:
            
               user=input('what do you want to do today \n Press: \n T - Transfer \n D - Depsoit \n W - Withdrawl\n B - Check Balance (: ' )
         
-              
+            #   transfer
               if user == 't':
                transfe=int(input('how much do you want to Transfer: '))
                transfdes=input('Description: ')
-               bank.transfer=transfe
-               bank.description=transfdes
-               r=func.transfer(bank.balance,bank.transfer,bank.description)
-               print(r)
-
+               print(r:=bank._transfer(transfe,transfdes))
+               
+            
+            #  deposit
               elif user == 'd':
                deposit=int(input('how much do you want to Deposit: '))
-               print(bank.deposit(deposit))
+               print(r:=bank._deposit(deposit))
+               
 
+            #   withdraw
               elif user == 'w':
                withdrawl=int(input('how much do you want to Withdrawl: '))
-               print(bank.withdrawl(withdrawl))
+               print(r:=bank._withdrawl(withdrawl))
 
+
+            #   checkbalance
               elif user == 'b':
-               print(bank.balance),
+               print(bank._balance),
           
 
-              d= input('do you want to proccess anything? y/n').lower()
-              if  d == 'y':
+              
+              if  (d:= input('do you want to process anything? y/n: ').lower()) !='y':
                break
             
 
